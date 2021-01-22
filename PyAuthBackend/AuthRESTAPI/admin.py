340099099c4b7f2e31e8bdc3ca523451bec9f886
AdrianoDiDio <AdrianoDD@8e86bfb0e784>
2021-01-22 22:54:40 +0100

    Admin Area:Added biometricChallenge display field

--- PyAuthBackend/AuthRESTAPI/admin.py
+++ PyAuthBackend/AuthRESTAPI/admin.py
@@ -11,6 +11,7 @@ class CustomUserAdmin(UserAdmin):
             {
                 'fields': (
                     'biometricToken',
+                    'biometricChallenge',
                 ),
             },
         ),
